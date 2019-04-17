ee2c81ebd6100420a0fddb53dafbf956558d851e
Alexey Dmytrenko <wow-alexey@mail.ru>
2019-04-18 00:31:22 +0300

    header footer done, started main content

--- dist/js/plugin.js
+++ dist/js/plugin.js
@@ -1,3 +1,10 @@
 $('.burger').on('click', function() {
     $('.general-container').toggleClass('opened');
+    $('body').toggleClass('overflow');
+});
+
+$('nav a').on('click', function () {
+    var currLink = $(this).data('link');
+    $('nav a').removeClass('active');
+    $('a[data-link="'+currLink+'"]').addClass('active');
 });
