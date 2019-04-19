d42f0c466b28acd18f93ccd6314950ede89c2a15
Alexey Dmitrenko <alexey.dmitrenko@bigdropinc.com>
2019-04-19 18:32:00 +0300

    need to do all articles

--- dist/js/plugin.js
+++ dist/js/plugin.js
@@ -14,13 +14,18 @@ $('.accordion').click(function(e) {
     e.preventDefault();
 
     var $this = $(this);
+    $('.accordion').removeClass('opened');
+
 
     if ($this.next().hasClass('show')) {
         $this.next().removeClass('show');
         $this.next().slideUp(350);
     } else {
+        $('.panel').removeClass('show');
+        $('.panel').slideUp(350);
         $this.parent().parent().find('li .panel').removeClass('show');
         $this.parent().parent().find('li .panel').slideUp(350);
+        $this.toggleClass('opened');
         $this.next().toggleClass('show');
         $this.next().slideToggle(350);
     }
