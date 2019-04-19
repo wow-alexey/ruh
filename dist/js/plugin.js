101cd2a9e556de62a7d2fad422fa0c053857c581
Alexey Dmytrenko <wow-alexey@mail.ru>
2019-04-20 02:13:24 +0300

    done

--- dist/js/plugin.js
+++ dist/js/plugin.js
@@ -30,3 +30,43 @@ $('.accordion').click(function(e) {
         $this.next().slideToggle(350);
     }
 });
+
+function appendHtml() {
+    if ($(window).width() < 640 && $('.intro-categories .container--tabs')){
+        $('.container--tabs').appendTo($('.for-tabs-container'));
+    } else if($(window).width() >= 640 && $('.for-tabs-container .container--tabs')) {
+        $('.container--tabs').appendTo($('.intro-categories'));
+    }
+}
+
+window.addEventListener("load", function() {
+    //tabs
+    var myTabs = document.querySelectorAll("ul.nav-tabs > li");
+    function myTabClicks(tabClickEvent) {
+        for (var i = 0; i < myTabs.length; i++) {
+            myTabs[i].classList.remove("active");
+        }
+        var clickedTab = tabClickEvent.currentTarget;
+        clickedTab.classList.add("active");
+        tabClickEvent.preventDefault();
+        var myContentPanes = document.querySelectorAll(".tab-pane");
+        for (i = 0; i < myContentPanes.length; i++) {
+            myContentPanes[i].classList.remove("active");
+        }
+        var anchorReference = tabClickEvent.target;
+        var activePaneId = anchorReference.getAttribute("href");
+        var activePane = document.querySelector(activePaneId);
+        activePane.classList.add("active");
+    }
+    for (var i = 0; i < myTabs.length; i++) {
+        myTabs[i].addEventListener("click", myTabClicks)
+    }
+});
+
+$( document ).ready(function() {
+    appendHtml();
+});
+
+$(window).resize(function() {
+    appendHtml();
+});
